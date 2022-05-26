6355e34dc44d90f7c1261e85f8a84a8e06ccb980
justin-chiu <justin-chiu@users.noreply.github.com>
2022-05-26 00:58:45 -0400

    v4.2
    • randomize node order
    • segment categories
    • order nodes and categories

--- js/denom-viz-scripts.js
+++ js/denom-viz-scripts.js
@@ -93,35 +93,73 @@ function deleteChar(str, chars = [","], from = "end") { // delete chars from sta
     return str;
 }
 
-function orderArray(data, dimension = dimensions.num, reverse = false) { // returns array with items ordered largest to smallest
+function orderArray(array, dimension = dimensions.num, reverse = false) { // false returns array with items ordered largest to smallest
 
-    data.sort(comparemasterData);
+    const sortFunction = sortByDimension(dimension);
+
+    array.sort(sortFunction);
 
     if (reverse) {
         data.reverse();
     }
 
-    return data;
+    return array;
 }
 
-function comparemasterData(objA, objB) { // compare function for Array.sort()
+// function compareSort(objA, objB) { // compare function for Array.sort()
 
-    const dimension = "Congregations"; // which dimension to sort by
+//     const dimension = dimensions.num; // which dimension to sort by
 
-    const valueA = objA[dimension];
-    const valueB = objB[dimension];
+//     const valueA = objA[dimension];
+//     const valueB = objB[dimension];
 
-    let compare = 0; // both values are equal
+//     let compare = 0; // both values are equal
 
-    if (valueA > valueB) {
-        compare = 1;
-    } else if (valueA < valueB) {
-        compare = -1;
+//     if (valueA > valueB) {
+//         compare = 1;
+//     } else if (valueA < valueB) {
+//         compare = -1;
+//     }
+
+//     return compare;
+
+// }
+
+function sortByDimension(dimension) {
+
+    return function (objA, objB) { // compare function for Array.sort()
+
+        const sortBy = dimension; // which dimension to sort by
+    
+        const valueA = objA[sortBy];
+        const valueB = objB[sortBy];
+    
+        let compare = 0; // both values are equal
+    
+        if (valueA > valueB) {
+            compare = 1;
+        } else if (valueA < valueB) {
+            compare = -1;
+        }
+    
+        return compare;
+    }
+} // for reference: https://www.sitepoint.com/sort-an-array-of-objects-in-javascript/
+
+function orderArrayRandom(array) {
+    let itemsLeft = array.length;
+
+    while (itemsLeft !== 0) {
+        let randIndex = Math.floor(Math.random() * itemsLeft);
+        itemsLeft--;
+        
+        let tempItem = JSON.parse(JSON.stringify(array[itemsLeft]));
+        array[itemsLeft] = array[randIndex];
+        array[randIndex] = tempItem;
     }
 
-    return compare;
-
-} // for reference: https://www.sitepoint.com/sort-an-array-of-objects-in-javascript/
+    return array;
+}
 
 function ghostNode(obj, objIndex, catIndex, counter) {
     // objIndex == index of object in dataset
@@ -138,7 +176,6 @@ function ghostNode(obj, objIndex, catIndex, counter) {
     let addAt = objIndex + counter + 1; // where to add the ghost node
     nodes.splice(addAt, 0, newGhost);
 }
-
 
 
 
@@ -191,24 +228,70 @@ function populatemasterData(data) { // places and sorts fetched content into mas
     return data;
 }
 
-function populateNodes(data) {
+function populateNodes(data, randomOrder = true) {
 
     let nodes = JSON.parse(JSON.stringify(data)); // set nodes as copy of data
 
-    categories = new Set(); // nodes for categories (faith traditions)
+    makeCategories(data);
+    segmentCategories(2);
+
+    for (let i = 0; i < categories.length; i++) { // add category (faith tradition) objects to nodes array
+
+        let catNode = {
+            id: "cat-" + categories[i].name,
+            type: "category"
+        }
+
+        catNode[dimensions.num] = 0;
+        catNode[dimensions.cat] = categories[i].name;
+
+        nodes.push(catNode);
+    }
+
+    nodes.push({ // add central node
+        id: "origin",
+        type: "origin"
+    });
+
+    if (randomOrder == true) {
+        nodes = orderArrayRandom(nodes);
+    }
+
+    return nodes;
+
+}
+
+function makeCategories(data) {
+
+    let catSet = new Set(); // list of unique categories
+    categories = new Array(); // global categories array
     let nodesAdded = 0; // counter
 
-    for (let j = 0; j < data.length; j++) {
+    for (let j = 0; j < data.length; j++) { // for every data obj
 
-        let objCats = data[j][dimensions.cat];
+        let newCat = data[j][dimensions.cat];
 
-        for (let k = 0; k < objCats.length; k++) { 
+        for (let k = 0; k < newCat.length; k++) { // for each categpry (faith tradition) in data obj
 
-            categories.add(objCats[k]); // add categories to categories set
+            if (catSet.has(newCat[k])) { // if category already in set
+
+                let catObj = categories.find(element => element.name == newCat[k]);
+                catObj.count++; // add to count
+
+            } else { // if category not in set
+
+                categories.push({ // create a category object
+                    name: newCat[k],
+                    count: 1
+                });
+
+            }
+
+            catSet.add(newCat[k]); // add category to set
 
             /* // GHOST NODES
 
-            if (objCats.length > 1) { // if multiple categories (faith traditions)
+            if (newCat.length > 1) { // if multiple categories (faith traditions)
                 ghostNode(data[j], j, k, nodesAdded); // create ghostNode
                 nodesAdded++; // how many ghost nodes were added
             }
@@ -216,29 +299,32 @@ function populateNodes(data) {
             */
         }
     }
+}
 
-    categories = Array.from(categories);
+function segmentCategories(segments, remainder = "largest_segment") {
 
-    for (let i = 0; i < categories.length; i++) { // add category (faith tradition) objects to nodes array
+    // order categories from least to greatest
+    categories = orderArray(categories, "count");
 
-        let newCatObj = {
-            id: "cat-" + categories[i],
-            type: "category"
+    let perSegment = Math.floor(categories.length / segments);
+
+    for (let i = 0; i < categories.length; i++) {
+
+        for (let j = 0; j < segments; j++) {
+            if ((i < perSegment * (j + 1)) && (i >= perSegment * j)) {
+
+                categories[i].segment = j;
+
+            } else if (i >= perSegment * segments) {
+
+                if (remainder == "largest_segment") {
+                    categories[i].segment = segments - 1;
+                } else {
+                    categories[i].segment = 0;
+                }
+            }
         }
-
-        newCatObj[dimensions.num] = 0;
-        newCatObj[dimensions.cat] = categories[i];
-
-        nodes.push(newCatObj);
     }
-
-    nodes.push({
-        id: "origin",
-        type: "origin"
-    });
-
-    return nodes;
-
 }
 
 function populateLinks(data, dimension = dimensions.cat) {
@@ -248,11 +334,11 @@ function populateLinks(data, dimension = dimensions.cat) {
     // every cat is linked to origin node
 
     for (let i = 0; i < categories.length; i++) {
-        
+
         let newLink = new Object();
         newLink.source = "origin";
-        newLink.target = "cat-" + categories[i];
-        newLink[dimensions.cat] = categories[i];
+        newLink.target = "cat-" + categories[i].name;
+        newLink[dimensions.cat] = categories[i].name;
 
         links.push(newLink);
     }
@@ -283,7 +369,7 @@ function populateLinks(data, dimension = dimensions.cat) {
 // ------FRONT-END------
 
 function setSVGDimensions(svg) // svg == element for which we are setting the width and height attributes
-{    
+{
     var w, h;
 
     w = svg.parentElement.clientWidth;
@@ -302,47 +388,47 @@ function defineViz() {
 
     // defining d3 force simulation
     forceLayout = d3.forceSimulation()
-    .force("link", d3.forceLink().id(function(d) { // links attract
-        return d.id;
-    })
-    .strength(1/3)
-    )
-    .force("charge", d3.forceManyBody() // nodes repel each other
-        .strength(-500)
-    )
-    .force("collision", d3.forceCollide()
-        .radius(function(d) {
-            return d.radius;
+        .force("link", d3.forceLink().id(function (d) { // links attract
+            return d.id;
         })
-    )
-    .force("center", d3.forceCenter(svgWidth / 2, svgHeight / 2))
-    .force("radial", d3.forceRadial()
-        .x(svgWidth / 2)
-        .y(svgHeight / 2)
-        .radius(function(d) {
-            switch (d.type) {
-                case "origin": return 0;
-                case "category": return 100;
-                case "affiliate": return 300;
-            }
-        })
-    )
-    ;
+            .strength(1 / 3)
+        )
+        .force("charge", d3.forceManyBody() // nodes repel each other
+            .strength(-500)
+        )
+        .force("collision", d3.forceCollide()
+            .radius(function (d) {
+                return d.radius;
+            })
+        )
+        .force("center", d3.forceCenter(svgWidth / 2, svgHeight / 2))
+        .force("radial", d3.forceRadial()
+            .x(svgWidth / 2)
+            .y(svgHeight / 2)
+            .radius(function (d) {
+                switch (d.type) {
+                    case "origin": return 0;
+                    case "category": return 100;
+                    case "affiliate": return 300;
+                }
+            })
+        )
+        ;
 
     // drawing links
     svgLink = svgD3.append("g")
-    .selectAll("line")
-    .data(vizLinks)
-    .enter().append("line")
+        .selectAll("line")
+        .data(vizLinks)
+        .enter().append("line")
         .classed("chart-link", true);
 
     // drawing nodes
     svgNode = svgD3.append("g")
-    .selectAll("circle")
-    .data(vizNodes)
-    .enter().append("circle")
+        .selectAll("circle")
+        .data(vizNodes)
+        .enter().append("circle")
         .classed("chart-node", true)
-        .attr("r", function(d) {
+        .attr("r", function (d) {
             if (d[dimensions.num] == 0) {
                 return 0;
             }
@@ -357,16 +443,16 @@ function defineViz() {
 function ticked() {
 
     svgLink
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+        .attr("x1", function (d) { return d.source.x; })
+        .attr("y1", function (d) { return d.source.y; })
+        .attr("x2", function (d) { return d.target.x; })
+        .attr("y2", function (d) { return d.target.y; });
 
     svgNode
         .attr("cx", function (d) {
             return d.x;
         })
-        .attr("cy", function(d) {
+        .attr("cy", function (d) {
             return d.y;
         });
 }
